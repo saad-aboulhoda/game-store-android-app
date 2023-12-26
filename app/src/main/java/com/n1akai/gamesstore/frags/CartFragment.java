bbbacb552cda93c7b0a80d4a326b53b2b9fea877
N1AKAI <rakansubs@gmail.com>
2023-12-26 20:05:10 +0100

    add auth checking, manage count, manage sub total and list cart

--- app/src/main/java/com/n1akai/gamesstore/frags/CartFragment.java
+++ app/src/main/java/com/n1akai/gamesstore/frags/CartFragment.java
@@ -2,7 +2,12 @@ package com.n1akai.gamesstore.frags;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -10,9 +15,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.n1akai.gamesstore.adapters.CartAdapter;
-import com.n1akai.gamesstore.models.Cart;
+import com.n1akai.gamesstore.models.CartItem;
 import com.n1akai.gamesstore.R;
 
 import java.util.ArrayList;
@@ -20,7 +33,13 @@ import java.util.ArrayList;
 public class CartFragment extends Fragment {
 
     View v;
-    ArrayList<Cart> carts;
+    TextView counts, totalGames, subTotal;
+    ArrayList<CartItem> cartItems;
+    FirebaseUser user;
+    DatabaseReference mRef;
+    NavController navController;
+    int allGamesCount = 0;
+    Double subTotalPrice = 0.0;
 
     public CartFragment() {
         // Required empty public constructor
@@ -32,30 +51,69 @@ public class CartFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         v = inflater.inflate(R.layout.fragment_cart, container, false);
-        cartList();
-        gamesCount();
         return v;
     }
 
-
-    private void cartList() {
-        carts = new ArrayList<>();
-        carts.add(new Cart("Kaze and the Wild Masks", "Go on a journey as Kaze in this 90’s classics inspired platformer. When the Crystal Islands get cursed, Kaze needs to save her friend Hogo while facing enraged living vegetables. Find the Wild Masks to unleash the powers of the legendary guardians and master land, sky and sea.", 5.99, R.drawable.new_release_img_3, 1));
-        RecyclerView rc = v.findViewById(R.id.rc_cart_list);
-        CartAdapter adapter = new CartAdapter(carts);
-        rc.setLayoutManager(new LinearLayoutManager(getActivity()));
-        rc.setAdapter(adapter);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        navController = Navigation.findNavController(v);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            mRef = FirebaseDatabase.getInstance().getReference("carts").child(user.getUid());
+            cartList();
+            initView();
+        } else {
+            Toast.makeText(getContext(), "You have to login first", Toast.LENGTH_SHORT).show();
+            navigateToUserLogin();
+        }
     }
 
-    private void gamesCount() {
-        TextView counts = v.findViewById(R.id.cart_et_games_count);
-        TextView totalGames = v.findViewById(R.id.cart_tv_total_games);
-        int sum = 0;
-        for (Cart cart: carts) {
-            sum += cart.getAmount();
-        }
-        counts.setText(sum+" games");
-        totalGames.setText(sum+" games");
+    private void initView() {
+        counts = v.findViewById(R.id.cart_et_games_count);
+        totalGames = v.findViewById(R.id.cart_tv_total_games);
+        subTotal = v.findViewById(R.id.cart_tv_sub_total);
+    }
+
+    private void cartList() {
+        cartItems = new ArrayList<>();
+        RecyclerView rc = v.findViewById(R.id.rc_cart_list);
+        CartAdapter adapter = new CartAdapter(cartItems);
+        rc.setLayoutManager(new LinearLayoutManager(getActivity()));
+        rc.setAdapter(adapter);
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot s: snapshot.getChildren()) {
+                    CartItem cartItem = s.getValue(CartItem.class);
+                    cartItems.add(cartItem);
+                    manageCount(cartItem);
+                    manageSubTotalPrice(cartItem);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void manageCount(CartItem cartItem) {
+        allGamesCount += cartItem.getAmount();
+        counts.setText(allGamesCount+" games");
+        totalGames.setText(allGamesCount+" games");
+    }
+
+    private void manageSubTotalPrice(CartItem cartItem) {
+        subTotalPrice += Double.parseDouble(cartItem.getPrice());
+        String subTotalPriceFormated = String.format("%.2f", subTotalPrice);
+        subTotal.setText("$"+subTotalPriceFormated);
+    }
+
+    private void navigateToUserLogin() {
+        NavDirections action = CartFragmentDirections.actionGlobalUserFragment();
+        navController.navigate(action);
     }
 
 }
