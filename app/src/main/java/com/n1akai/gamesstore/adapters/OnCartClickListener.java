97f884c0507c30855642cedc39ab618ed2ca58d4
N1AKAI <rakansubs@gmail.com>
2023-12-26 20:32:40 +0100

    add two views

--- app/src/main/java/com/n1akai/gamesstore/adapters/OnCartClickListener.java
+++ app/src/main/java/com/n1akai/gamesstore/adapters/OnCartClickListener.java
@@ -1,7 +1,9 @@
 package com.n1akai.gamesstore.adapters;
 
+import android.view.View;
+
 import com.n1akai.gamesstore.models.Game;
 
 public interface OnCartClickListener {
-    void onCartClick(Game game);
+    void onCartClick(Game game, View cart, View check);
 }
