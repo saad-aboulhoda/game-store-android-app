cbb13ec136806d0765e85479f2b9aa630336a445
N1AKAI <rakansubs@gmail.com>
2023-12-26 19:42:12 +0100

    change name to CartItem

---
+++ app/src/main/java/com/n1akai/gamesstore/models/CartItem.java
@@ -0,0 +1,52 @@
+package com.n1akai.gamesstore.models;
+
+public class CartItem {
+    private String id;
+    private String title;
+    private String story;
+    private String price;
+    private String img;
+    private int amount;
+
+
+
+    public CartItem() {
+    }
+
+    public CartItem(String id, String title, String story, String price, String img) {
+        this.id = id;
+        this.title = title;
+        this.story = story;
+        this.price = price;
+        this.img = img;
+        this.amount = 1;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getStory() {
+        return story;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
