7c27b180e8dad5ba5b4d9c0536749d4764c15f72
N1AKAI <rakansubs@gmail.com>
2023-12-26 14:30:28 +0100

    merge

--- app/src/main/java/com/n1akai/gamesstore/Home.java
+++ app/src/main/java/com/n1akai/gamesstore/Home.java
@@ -42,6 +42,7 @@ public class Home extends AppCompatActivity {
         setupBottomNav();
         hideHomeHeaderInOtherFragments();
         navigateToSearchFrag();
+        // Testing
         // new feature
     }
 
