7efdee671821c5c2a8d6c9fd1892987b83daee8c
N1AKAI <rakansubs@gmail.com>
2023-12-27 13:03:25 +0100

    change cart to cartBtn

--- app/src/main/java/com/n1akai/gamesstore/adapters/OnCartBtnClickListener.java
+++ app/src/main/java/com/n1akai/gamesstore/adapters/OnCartBtnClickListener.java
@@ -1,10 +1,9 @@
 package com.n1akai.gamesstore.adapters;
 
-import android.view.View;
 import android.widget.Button;
 
 import com.n1akai.gamesstore.models.Game;
 
-public interface OnCartClickListener {
+public interface OnCartBtnClickListener {
     void onCartClick(Game game, Button cart, Button check);
 }
